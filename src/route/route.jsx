a385496ede490e11ea86ebd4df6319374b95ab18
iNeelPatel <neelpatel.6531@icloud.com>
2018-08-21 04:07:43 +0530

    add dashboard

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "../auth/component/login";
+import Dashboard from "../dashboard/component";
 const Routepath = () => (
   <Router>
     <div>
       <Route exact path="/" component={Login} />
+      <Route path="/user/dashboard" component={Dashboard} />
     </div>
   </Router>
 );
