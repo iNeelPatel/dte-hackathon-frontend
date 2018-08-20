dd2afcfe820a3a55c0f2b7baeb655d5d2a04c93e
thobhanifreddy <thobhanifreddy@gmail.com>
2018-08-21 02:49:41 +0530

    connected parse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import Parse from "parse";
+require("dotenv").config();
+
+Parse.initialize(
+  process.env.REACT_APP_PARSE_APP,
+  process.env.REACT_APP_PARSE_JAVASCRIPT_KEY,
+  process.env.REACT_APP_PARSE_MASTER_KEY
+);
+
+Parse.serverURL = process.env.REACT_APP_PARSE_SERVER;
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
