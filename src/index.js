0d035332336597207ae18c9638eaa71fd89a3f40
iNeelPatel <neelpatel.6531@icloud.com>
2018-08-21 02:54:12 +0530

    Login Form Antd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Routepath from "./auth/user/component/login";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Routepath />, document.getElementById("root"));
 registerServiceWorker();
