e4e21be2a7f9ffed020ef2f39af474c9170812f8
thobhanifreddy <thobhanifreddy@gmail.com>
2018-08-21 00:36:03 +0530

    parse setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Parse from "parse";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  appId,
+  javascriptKey,
+  serverUrl,
+  masterKey
+} from "./config/ParseServerConfig";
+import DvaConfig from "./config/DvaConfig";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+Parse.initialize(appId, javascriptKey, masterKey);
+
+Parse.serverURL = serverUrl;
+
+DvaConfig.start("#react");
 registerServiceWorker();
