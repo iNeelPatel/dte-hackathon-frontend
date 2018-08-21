f7694ec73e6150f35c16b9fecb6372d18742157e
iNeelPatel <neelpatel.6531@icloud.com>
2018-08-22 01:09:49 +0530

    navbar

--- src/modules/header/components/header.jsx
+++ src/modules/header/components/header.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import { Menu } from "antd";
 import "./header.css";
 import "antd/dist/antd.css";
+import MenuItem from "antd/lib/menu/MenuItem";
 
 const HeaderNav = () => {
   return (
     <Menu onClick={this.handleClick} mode="horizontal">
-      <img src="/assets/dte.png" alt="DTE Logo" className="dte-logo-nav" />
-      <div className="nav">navBar</div>
+      <MenuItem>
+        <img src="/assets/dte.png" alt="DTE Logo" className="dte-logo-nav" />
+      </MenuItem>
+      <Menu.Item key="/user/dashboard">Dashboard</Menu.Item>
+      <Menu.Item key="/user/inwardOutwardRegister">
+        Inward/Outward Register
+      </Menu.Item>
+      <Menu.Item key="/user/institute">Institute</Menu.Item>
     </Menu>
   );
 };
