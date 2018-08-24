40ec602d2530a02784968b0aadc1554420b3dcfd
thobhanifreddy <thobhanifreddy@gmail.com>
2018-08-24 22:52:15 +0530

    fetching institute data

--- src/modules/institute/components/screen.jsx
+++ src/modules/institute/components/screen.jsx
@@ -1,10 +1,42 @@
 import React from "react";
-import { Layout, Icon } from "antd";
+import { Layout, Icon, Row, Col, Table } from "antd";
 import "antd/dist/antd.css";
 import PageHeader from "ant-design-pro/lib/PageHeader";
 import "ant-design-pro/dist/ant-design-pro.css";
 import "./screen.css";
-import InstituteFormContainerWithForm from "../container/form";
+
+import InstituteFormContainer from "../container/form";
+
+const getColumns = () => [
+  {
+    title: "Code",
+    dataIndex: "code",
+    key: "code"
+  },
+
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name"
+  },
+  {
+    title: "Type",
+    dataIndex: "type",
+    key: "type"
+  }
+
+  // {
+  //   title: "Edit",
+  //   key: "edit",
+  //   render: (edit, institute) => (
+  //     <InstituteFormContainer
+  //       handleChange={{ handleUpdate, handleDelete }}
+  //       edit={true}
+  //       institute={institute}
+  //     />
+  //   )
+  // }
+];
 
 const InstituteComponent = props => {
   console.log(props);
@@ -15,10 +47,23 @@ const InstituteComponent = props => {
         logo={<Icon type="home" style={{ fontSize: "21px" }} />}
         action={
           <div>
-            <InstituteFormContainerWithForm />
+            <InstituteFormContainer />
           </div>
         }
       />
+      <Layout.Content style={{ marginTop: 20 }}>
+        <Row>
+          <Col span="24">
+            <Table
+              dataSource={props.institutes}
+              bordered
+              loading={props.loading}
+              // columns={getColumns(props.handleUpdate, props.handleDelete)}
+              // key={props.institute.key}
+            />
+          </Col>
+        </Row>
+      </Layout.Content>
     </Layout>
   );
 };
--- src/modules/institute/container/screen.jsx
+++ src/modules/institute/container/screen.jsx
@@ -1,17 +1,48 @@
 import React, { Component } from "react";
 import { connect } from "dva";
+import { message } from "antd";
+import { routerRedux } from "dva/router";
+import Parse from "parse";
 
-import InstituteComponet from "../components/screen";
+import InstituteScreenComponet from "../components/screen";
 
 class InstituteContainer extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      laoding: true,
+      institutes: []
+    };
   }
+
+  componentDidMount() {
+    const instituteQuery = new Parse.Query("Institute");
+    this.setState({ loading: true });
+
+    instituteQuery
+      .find()
+      .then(institutes => {
+        this.setState({
+          loading: false,
+          institutes: institutes.map(institute => ({
+            id: institute.id,
+            parseObject: institute,
+            key: institute.id,
+            ...institute.toJSON()
+          }))
+        });
+      })
+      .catch(error => message.error(error.message()));
+  }
+
   render() {
-    console.log("here");
-    return <InstituteComponet />;
+    return (
+      <InstituteScreenComponet
+        institutes={this.state.institutes}
+        loading={this.state.loading}
+      />
+    );
   }
 }
 
